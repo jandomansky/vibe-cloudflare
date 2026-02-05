d9763917bdc09dd29411b7c8f6f3c9d5a712c10a
jandomansky <jan.domansky@metrostav.cz>
2026-02-05 13:06:23 +0100

    Update objects.js

--- functions/api/objects.js
+++ functions/api/objects.js
@@ -123,7 +123,10 @@ const prompt =
 function json(data, status = 200) {
   return new Response(JSON.stringify(data, null, 2), {
     status,
-    headers: { "content-type": "application/json; charset=utf-8" },
+    headers: {
+      "content-type": "application/json; charset=utf-8",
+      "cache-control": "no-store"
+    }
   });
 }
 
